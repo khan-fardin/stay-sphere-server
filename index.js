3b1c35bf4d0246ae0ab36c73f0ee11bf93c2f26b
khan-fardin <1120f.khan@gmail.com>
2025-06-13 00:23:37 +0600

    get individually

--- index.js
+++ index.js
@@ -36,6 +36,14 @@ async function run() {
             res.send(result);
         });
 
+        // get individually
+        app.get('/rooms/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await roomCollection.findOne(query);
+            res.send(result);
+        })
+
         // Send a ping to confirm a successful connection
         // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
